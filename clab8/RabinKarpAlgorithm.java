59b716a0b7826db263f085f1e0399a7c998bef89
Akshay Gupta <akshaygrkesh@gmail.com>
2020-12-22 11:48:10 +0530

    Challenge Lab 8 Q2

--- clab8/RabinKarpAlgorithm.java
+++ clab8/RabinKarpAlgorithm.java
@@ -1,12 +1,29 @@
 public class RabinKarpAlgorithm {
 
+  /**
+   * This algorithm returns the starting index of the matching substring.
+   * This method will return -1 if no matching substring is found, or if the
+   * input is invalid.
+   */
+  public static int rabinKarp(String input, String pattern) {
+    int index = 0;
+    int inputLength = input.length();
+    int patternLength = pattern.length();
 
-    /**
-     * This algorithm returns the starting index of the matching substring.
-     * This method will return -1 if no matching substring is found, or if the input is invalid.
-     */
-    public static int rabinKarp(String input, String pattern) {
-        return -1;
+    if (inputLength == 0 || inputLength < patternLength)
+      return -1;
+
+    RollingString patternToMatch = new RollingString(pattern, patternLength);
+    int patternHash = patternToMatch.hashCode();
+    RollingString rollingInput =
+        new RollingString(input.substring(0, patternLength), patternLength);
+    while (index + inputLength < patternLength) {
+      if (rollingInput.hashCode() == patternHash &&
+          rollingInput.equals(patternToMatch))
+        return index;
+      rollingInput.addChar(input.charAt(index + inputLength));
+      index++;
     }
-
+    return -1;
+  }
 }
--- clab8/RollingString.java
+++ clab8/RollingString.java
@@ -1,80 +1,95 @@
+import java.util.LinkedList;
+
 /**
- * A String-like class that allows users to add and remove characters in the String
- * in constant time and have a constant-time hash function. Used for the Rabin-Karp
- * string-matching algorithm.
+ * A String-like class that allows users to add and remove characters in the
+ * String in constant time and have a constant-time hash function. Used for the
+ * Rabin-Karp string-matching algorithm.
  */
-class RollingString{
+class RollingString {
 
-    /**
-     * Number of total possible int values a character can take on.
-     * DO NOT CHANGE THIS.
-     */
-    static final int UNIQUECHARS = 128;
+  /**
+   * Number of total possible int values a character can take on.
+   * DO NOT CHANGE THIS.
+   */
+  static final int UNIQUECHARS = 128;
 
-    /**
-     * The prime base that we are using as our mod space. Happens to be 61B. :)
-     * DO NOT CHANGE THIS.
-     */
-    static final int PRIMEBASE = 6113;
+  /**
+   * The prime base that we are using as our mod space. Happens to be 61B. :)
+   * DO NOT CHANGE THIS.
+   */
+  static final int PRIMEBASE = 6113;
 
-    /**
-     * Initializes a RollingString with a current value of String s.
-     * s must be the same length as the maximum length.
-     */
-    public RollingString(String s, int length) {
-        assert(s.length() == length);
-        /* FIX ME */
+  /**
+   * Initializes a RollingString with a current value of String s.
+   * s must be the same length as the maximum length.
+   */
+
+  private LinkedList<Character> strList;
+  private int fixedLength;
+  private int unModdedHash;
+
+  private int calcHashCode(String s, int length) {
+    int sum = 0;
+    for (int i = 0; i < length; i++) {
+      sum += s.charAt(i) * Math.pow(UNIQUECHARS, i);
     }
+    return sum;
+  }
 
-    /**
-     * Adds a character to the back of the stored "string" and 
-     * removes the first character of the "string". 
-     * Should be a constant-time operation.
-     */
-    public void addChar(char c) {
-        /* FIX ME */
+  public RollingString(String s, int length) {
+    assert (s.length() == length);
+    LinkedList<Character> lst = new LinkedList<Character>(s);
+    fixedLength = length;
+    unModdedHash = calcHashCode(s, length);
+  }
+
+  /**
+   * Adds a character to the back of the stored "string" and
+   * removes the first character of the "string".
+   * Should be a constant-time operation.
+   */
+  public void addChar(char c) {
+    char removed = strList.removeFirst();
+    strList.addLast(c);
+    unModdedHash -= removed * Math.pow(UNIQUECHARS, fixedLength - 1);
+    unModdedHash = (unModdedHash * UNIQUECHARS) + c;
+  }
+
+  /**
+   * Returns the "string" stored in this RollingString, i.e. materializes
+   * the String. Should take linear time in the number of characters in
+   * the string.
+   */
+  public String toString() {
+    StringBuilder strb = new StringBuilder();
+    for (Character ch : strList) {
+      strb.append(ch);
     }
+    return strb.toString();
+  }
 
+  /**
+   * Returns the fixed length of the stored "string".
+   * Should be a constant-time operation.
+   */
+  public int length() { return fixedLength; }
 
-    /**
-     * Returns the "string" stored in this RollingString, i.e. materializes
-     * the String. Should take linear time in the number of characters in
-     * the string.
-     */
-    public String toString() {
-        StringBuilder strb = new StringBuilder();
-        /* FIX ME */
-        return "";
-    }
+  /**
+   * Checks if two RollingStrings are equal.
+   * Two RollingStrings are equal if they have the same characters in the same
+   * order, i.e. their materialized strings are the same.
+   */
+  @Override
+  public boolean equals(Object o) {
+    return (this.toString()).equals(o.toString());
+  }
 
-    /**
-     * Returns the fixed length of the stored "string".
-     * Should be a constant-time operation.
-     */
-    public int length() {
-        /* FIX ME */
-        return -1;
-    }
-
-
-    /**
-     * Checks if two RollingStrings are equal.
-     * Two RollingStrings are equal if they have the same characters in the same
-     * order, i.e. their materialized strings are the same.
-     */
-    @Override
-    public boolean equals(Object o) {
-        /* FIX ME */
-        return false;
-    }
-
-    /**
-     * Returns the hashcode of the stored "string".
-     * Should take constant time.
-     */
-    @Override
-    public int hashCode() {
-        /* FIX ME */
-        return -1;
-    }
+  /**
+   * Returns the hashcode of the stored "string".
+   * Should take constant time.
+   */
+  @Override
+  public int hashCode() {
+    return unModdedHash % PRIMEBASE;
+  }
 }
