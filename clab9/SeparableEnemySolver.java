1b9bbd8f26506455fdba6bc0a6cb9118411a0788
Akshay Gupta <akshaygrkesh@gmail.com>
2021-01-12 20:28:08 +0530

    Challenge Lab 9

--- clab9/SeparableEnemySolver.java
+++ clab9/SeparableEnemySolver.java
@@ -4,86 +4,108 @@ import java.util.*;
 
 public class SeparableEnemySolver {
 
-    Graph g;
+  Graph g;
 
-    /**
-     * Creates a SeparableEnemySolver for a file with name filename. Enemy
-     * relationships are biderectional (if A is an enemy of B, B is an enemy of A).
-     */
-    SeparableEnemySolver(String filename) throws java.io.FileNotFoundException {
-        this.g = graphFromFile(filename);
-    }
+  /**
+   * Creates a SeparableEnemySolver for a file with name filename. Enemy
+   * relationships are biderectional (if A is an enemy of B, B is an enemy of A).
+   */
+  SeparableEnemySolver(String filename) throws java.io.FileNotFoundException {
+    this.g = graphFromFile(filename);
+  }
 
-    /** Alterntive constructor that requires a Graph object. */
-    SeparableEnemySolver(Graph g) {
-        this.g = g;
-    }
+  /** Alterntive constructor that requires a Graph object. */
+  SeparableEnemySolver(Graph g) {
+    this.g = g;
+  }
 
-    /**
-     * Returns true if input is separable, false otherwise.
-     */
-    public boolean isSeparable() {
-        // TODO: Fix me
-        return false;
-    }
+  /**
+   * Returns true if input is separable, false otherwise.
+   */
+  public boolean isSeparable() {
+    Iterator<String> attendees = g.labels().iterator();
+    HashSet<String> group1 = new HashSet<>();
+    HashSet<String> group2 = new HashSet<>();
 
-
-    /* HELPERS FOR READING IN CSV FILES. */
-
-    /**
-     * Creates graph from filename. File should be comma-separated. The first line
-     * contains comma-separated names of all people. Subsequent lines each have two
-     * comma-separated names of enemy pairs.
-     */
-    private Graph graphFromFile(String filename) throws FileNotFoundException {
-        List<List<String>> lines = readCSV(filename);
-        Graph input = new Graph();
-        for (int i = 0; i < lines.size(); i++) {
-            if (i == 0) {
-                for (String name : lines.get(i)) {
-                    input.addNode(name);
-                }
-                continue;
-            }
-            assert(lines.get(i).size() == 2);
-            input.connect(lines.get(i).get(0), lines.get(i).get(1));
+    while (attendees.hasNext()) {
+      String next = attendees.next();
+      Set<String> enemies = g.neighbors(next);
+      Set<String> enemiesInGroupOne = new HashSet<String>(enemies);
+      enemiesInGroupOne.retainAll(group1);
+      Set<String> enemiesInGroupTwo = new HashSet<String>(enemies);
+      enemiesInGroupTwo.retainAll(group2);
+      if (enemiesInGroupOne.isEmpty()) {
+        group1.add(next);
+        group2.addAll(enemies);
+      } else {
+        if (enemiesInGroupTwo.isEmpty()) {
+          group2.add(next);
+          group1.addAll(enemies);
+        } else {
+          return false;
         }
-        return input;
+      }
     }
+    return true;
+  }
+  // can also use a disjoint set, and connect all enemies per vertex
+  // and then query if vertex is connected to one of its enemies
+  // what about if you can use three groups??
 
-    /**
-     * Reads an entire CSV and returns a List of Lists. Each inner
-     * List represents a line of the CSV with each comma-seperated
-     * value as an entry. Assumes CSV file does not contain commas
-     * except as separators.
-     * Returns null if invalid filename.
-     *
-     * @source https://www.baeldung.com/java-csv-file-array
-     */
-    private List<List<String>> readCSV(String filename) throws java.io.FileNotFoundException {
-        List<List<String>> records = new ArrayList<>();
-        Scanner scanner = new Scanner(new File(filename));
-        while (scanner.hasNextLine()) {
-            records.add(getRecordFromLine(scanner.nextLine()));
+  /* HELPERS FOR READING IN CSV FILES. */
+
+  /**
+   * Creates graph from filename. File should be comma-separated. The first line
+   * contains comma-separated names of all people. Subsequent lines each have two
+   * comma-separated names of enemy pairs.
+   */
+  private Graph graphFromFile(String filename) throws FileNotFoundException {
+    List<List<String>> lines = readCSV(filename);
+    Graph input = new Graph();
+    for (int i = 0; i < lines.size(); i++) {
+      if (i == 0) {
+        for (String name : lines.get(i)) {
+          input.addNode(name);
         }
-        return records;
+        continue;
+      }
+      assert (lines.get(i).size() == 2);
+      input.connect(lines.get(i).get(0), lines.get(i).get(1));
     }
+    return input;
+  }
 
-    /**
-     * Reads one line of a CSV.
-     *
-     * @source https://www.baeldung.com/java-csv-file-array
-     */
-    private List<String> getRecordFromLine(String line) {
-        List<String> values = new ArrayList<String>();
-        Scanner rowScanner = new Scanner(line);
-        rowScanner.useDelimiter(",");
-        while (rowScanner.hasNext()) {
-            values.add(rowScanner.next().trim());
-        }
-        return values;
+  /**
+   * Reads an entire CSV and returns a List of Lists. Each inner List represents a
+   * line of the CSV with each comma-seperated value as an entry. Assumes CSV file
+   * does not contain commas except as separators. Returns null if invalid
+   * filename.
+   *
+   * @source https://www.baeldung.com/java-csv-file-array
+   */
+  private List<List<String>> readCSV(String filename) throws java.io.FileNotFoundException {
+    List<List<String>> records = new ArrayList<>();
+    Scanner scanner = new Scanner(new File(filename));
+    while (scanner.hasNextLine()) {
+      records.add(getRecordFromLine(scanner.nextLine()));
     }
+    return records;
+  }
 
-    /* END HELPERS  FOR READING IN CSV FILES. */
+  /**
+   * Reads one line of a CSV.
+   *
+   * @source https://www.baeldung.com/java-csv-file-array
+   */
+  private List<String> getRecordFromLine(String line) {
+    List<String> values = new ArrayList<String>();
+    Scanner rowScanner = new Scanner(line);
+    rowScanner.useDelimiter(",");
+    while (rowScanner.hasNext()) {
+      values.add(rowScanner.next().trim());
+    }
+    return values;
+  }
 
+  /* END HELPERS FOR READING IN CSV FILES. */
 }
